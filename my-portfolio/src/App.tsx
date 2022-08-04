582095803e5d765b4e60b8682c44c40aa5f205f2
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-08-04 17:54:56 -0300

    iniciando a estrutura do html

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -3,7 +3,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Teste</p>
+      
+      {/* inicio da secao home */}
+      <h1>
+        <a href="">Bem Vindo</a>
+      </h1>
+      <p>Thiago Fernandes</p>
+      <p>Desenvolvedor Front-End</p>
+
+      <nav>
+        <ul>
+          <li>&lt;Home&gt;</li>
+          <li>&lt;Sobre&gt;</li>
+          <li>&lt;Portfolio&gt;</li>
+          <li>&lt;Educação&gt;</li>
+          <li>&lt;Experiencia&gt;</li>
+          <li>&lt;Contato&gt;</li>
+        </ul>
+      </nav>
+
+      <button>Baixar Currículo</button>
+      <button>Contratar</button>
+      {/* fim da secao home */}
+
+      {/* inicio da secao sobre */}
+      {/* fim da secao sobre */}
+
+      {/* inicio da secao portfolio */}
+      {/* fim da secao portfolio */}
+
+      {/* inicio da secao educacao */}
+      {/* fim da secao educacao */}
+
+      {/* inicio da secao experiencia */}
+      {/* fim da secao experiencia */}
+
+      {/* inicio da secao contato */}
+      {/* fim da secao contato */}
+
     </div>
   );
 }
