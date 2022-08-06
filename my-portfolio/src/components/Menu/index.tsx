0c4ead7acf32d5fe31929de95be4a94eb4a528a3
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-08-06 15:07:12 -0300

    componente de menu mobile funcionando e animado

--- my-portfolio/src/components/Menu/index.tsx
+++ my-portfolio/src/components/Menu/index.tsx
@@ -2,7 +2,8 @@ import { memo, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { GrClose } from 'react-icons/gr';
 import styles from './Menu.module.scss';
-
+//menu true icone fechar - classOpen
+//menu false icone hamburguer - classClose
 function Menu() {
 
   const[menuMobileState, setMenuMobileState] = useState(false);
@@ -11,32 +12,42 @@ function Menu() {
     <>
       <div 
         onClick={() => setMenuMobileState(!menuMobileState)}
-        className={styles.hamburguer}
+        className={menuMobileState
+          ? styles.hamburguerOpen
+          : styles.hamburguerClose
+        }
       >
         {menuMobileState 
           ? <GrClose className={styles.hamburguer__icon}/>
-          : <FaBars className={styles.hamburguer__icon}/>
+          : <FaBars color='#000'className={styles.hamburguer__icon}/>
         }        
       </div>
 
-      <aside className={styles.menu}>
-        <h1 className={styles.menu__welcome}>
-          <a href="">Bem Vindo!</a>
-        </h1>          
-        <p className={styles.menu__content}>Thiago Fernandes</p>
-        <p className={styles.menu__content}>Desenvolvedor Front-End</p>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Sobre</li>
-            <li>Portfolio</li>
-            <li>Educação</li>
-            <li>Experiencia</li>
-            <li>Contato</li>
+      <aside className={menuMobileState
+        ? styles.menu
+        : styles.menuClose
+      }>
+        <div className={styles.menu__container}>
+          <h1 className={styles.menu__container___welcome}>
+            <a className={styles.menu__title}href="">Bem Vindo!</a>
+          </h1>
+          <p className={styles.menu__container___content}>Thiago Fernandes</p>
+          <p className={styles.menu__container___content}>Desenvolvedor Front-End</p>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.nav__list}>
+            <li className={styles.nav__list___item}>Home</li>
+            <li className={styles.nav__list___item}>Sobre</li>
+            <li className={styles.nav__list___item}>Portfolio</li>
+            <li className={styles.nav__list___item}>Educação</li>
+            <li className={styles.nav__list___item}>Experiencia</li>
+            <li className={styles.nav__list___item}>Contato</li>
           </ul>
         </nav>
-        <button>Baixar Currículo</button>
-        <button>Contratar</button>
+        <div className={styles.menu__container}>
+          <button className={styles.menu__container___button}>Baixar Currículo</button>
+          <button className={styles.menu__container___button}>Contratar</button>
+        </div>
       </aside>
     </>
   );
---
+++ my-portfolio/src/framerAnimation/variants.ts
@@ -0,0 +1,14 @@
+export const menuMobileVariants = {
+  initial : {
+    x: '-100vw',
+  },
+  animate: {
+    x: 0,
+    transition: {
+      duration: 1,
+      type: 'spring',
+      stiffness: 150
+    }
+  }
+};
+ 
