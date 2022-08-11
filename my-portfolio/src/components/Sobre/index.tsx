82c4fc74275f8f719872dd82031485b1efc1e0fd
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-08-11 13:25:43 -0300

    componente sobre refatorado

--- my-portfolio/src/components/Sobre/index.tsx
+++ my-portfolio/src/components/Sobre/index.tsx
@@ -1,12 +1,7 @@
 import styles from './Sobre.module.scss';
 import Me from 'assets/campeao.jpg';
-import React from 'assets/stacks/react.png';
-import Typescript from 'assets/stacks/typescript.png';
-import Javascript from 'assets/stacks/js.png';
-import Sass from 'assets/stacks/sass.png';
-import Git from 'assets/stacks/git.png';
-import Css3 from 'assets/stacks/css3.png';
-import HTML from 'assets/stacks/html.png';
+
+import data from './data.json';
 
 export default function Sobre() {
   return (
@@ -24,41 +19,23 @@ export default function Sobre() {
             <h2 className={styles.about__container___containerStacks____title}>
               Tecnologias que venho usando e desenvolvendo minhas habilidades atualmente:
             </h2>
-            
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={React} alt="Icone React" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>React</h3>
-            </div>
 
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={Typescript} alt="Icone Typescript" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>Typescript</h3>
-            </div>
-
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={Javascript} alt="Icone Javascript" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>Javascript</h3>
-            </div>
-
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={Sass} alt="Icone Sass" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>Sass</h3>
-            </div>
-
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={Git} alt="Icone Git" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>Git</h3>
-            </div>
-
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={Css3} alt="Icone Css3" className={styles.about__container___containerStacks____card_____icon}/>
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>Css3</h3>
-            </div>
-
-            <div className={styles.about__container___containerStacks____card}>{/* card da tech */}
-              <img src={HTML} alt="Icone HTML5" className={styles.about__container___containerStacks____card_____icon}/>              
-              <h3 className={styles.about__container___containerStacks____card_____subtitle}>HTML5</h3>
-            </div>   
+            {
+              data.map((item, index) => (
+                <div 
+                  key={index}
+                  className={styles.about__container___containerStacks____card}
+                >
+                  <img 
+                    src={item.src} 
+                    alt={item.alt} 
+                    className={styles.about__container___containerStacks____card_____icon}
+                  />
+                  <h3 className={styles.about__container___containerStacks____card_____subtitle}>   {item.content}
+                  </h3>
+                </div>
+              ))
+            } 
           </div>
         </div>
       </section>
